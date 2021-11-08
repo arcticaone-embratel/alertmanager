fb52e923f66d0032b18168eb5e74bb3d54b12557
arcticaone-embratel <paulo.bianchi@arctica.one>
2021-11-08 10:25:07 -0300

    alterado descricao menu / portugues

--- commit_git.sh
+++ commit_git.sh
@@ -2,5 +2,6 @@
 git add --all
 #git add appserver/static/incident_posture_novo.js
 #git add appserver/static/alert_manager_icon.png
-git commit -m "first commit"
+#colocar abaixo a msg do q foi alterado antes de comitar...
+git commit -m "alterado descricao menu / portugues"
 git push -u origin main
