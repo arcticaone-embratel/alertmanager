724948c7fe5ca1778d1c4a207047e8bb403fdc43
arcticaone-embratel <paulo.bianchi@arctica.one>
2021-11-11 10:03:40 -0300

    11nov21 - bkp_inicial_dial

--- appserver/static/incidente_posture_custom/js/clearTokens.js
+++ appserver/static/incidente_posture_custom/js/clearTokens.js
@@ -6,13 +6,11 @@ require([
 ], function(mvc, _, $) {
 
     $(document).ready(function() {
-    // executes when HTML-Document is loaded and DOM is ready
-        //alert('teste');
         $(".show-global-filters").val('mostrar filtros');
     });
 
-    //
-    
+    var defaultTokens = mvc.Components.get("default");
+    var submittedTokens = mvc.Components.get("submitted");
     
     var inputs = Object.keys(mvc.Components.attributes).filter(function(val) {
         return /customInput[A-Za-z0-9]+$/i.test(val);
@@ -22,12 +20,16 @@ require([
     customButton.attr("class", "btn btn-primary");
     customButton.text("Limpar Filtros");
 
-    customButton.on("click", function(){
+    customButton.on("click", function(){      
+
         for (var i=0; i < inputs.length; i++) {
 
             var currentInput = mvc.Components.get(inputs[i]);
             currentInput.val(currentInput.settings.attributes.default);
         }
+
+        submittedTokens.set("urlSearch", ""); 
+        defaultTokens.set("urlSearch", "");
     });
 
     var clearTokensButton = $("<div/>");
--- commit_git.sh
+++ commit_git.sh
@@ -4,5 +4,5 @@
 #linha abaixo -all varre todos arquivos modificados
 git add --all
 #colocar abaixo a msg do q foi alterado antes de comitar...
-git commit -m "10nov21 - upload - bkp"
+git commit -m "11nov21 - bkp_inicial_dial"
 git push -u origin main
