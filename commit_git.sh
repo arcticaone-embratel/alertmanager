00e04e1b1fcbc4ff0ec6d5a0fda4c9b9fc804c89
arcticaone-embratel <paulo.bianchi@arctica.one>
2021-11-11 14:17:57 -0300

    11nov21 - exclusao jobs bdis dentro app alertmanager -> movido p/ search igual prod

--- commit_git.sh
+++ commit_git.sh
@@ -4,5 +4,5 @@
 #linha abaixo -all varre todos arquivos modificados
 git add --all
 #colocar abaixo a msg do q foi alterado antes de comitar...
-git commit -m "11nov21 - bkp_inicial_dial"
+git commit -m "11nov21 - exclusao jobs bdis dentro app alertmanager -> movido p/ search igual prod"
 git push -u origin main
