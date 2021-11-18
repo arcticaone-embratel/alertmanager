cf1e088da4392d94e64bdf0bf7ee3527d45b3b22
arcticaone-embratel <paulo.bianchi@arctica.one>
2021-11-18 09:06:18 -0300

    18nov21 - bkp inicial dia

--- commit_git.sh
+++ commit_git.sh
@@ -4,5 +4,5 @@
 #linha abaixo -all varre todos arquivos modificados
 git add --all
 #colocar abaixo a msg do q foi alterado antes de comitar...
-git commit -m "17nov21 - alterada contagem do up"
+git commit -m "18nov21 - bkp inicial dia"
 git push -u origin main
