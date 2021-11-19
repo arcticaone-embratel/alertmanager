8017423b60822982f5f6ea49a4b939f464537777
arcticaone-embratel <paulo.bianchi@arctica.one>
2021-11-19 09:55:32 -0300

    19nov21 - painel gerencial -> busca por codprime / adaptado macros / dash principal

--- commit_git.sh
+++ commit_git.sh
@@ -4,5 +4,5 @@
 #linha abaixo -all varre todos arquivos modificados
 git add --all
 #colocar abaixo a msg do q foi alterado antes de comitar...
-git commit -m "18nov21 - alterado csv graf multiplan"
+git commit -m "19nov21 - painel gerencial -> busca por codprime / adaptado macros / dash principal"
 git push -u origin main
