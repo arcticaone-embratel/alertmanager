5cbe38ae7acff422127b8aa9f47f7d90562e6c94
arcticaone-embratel <paulo.bianchi@arctica.one>
2021-12-02 13:59:33 -0300

    02dez21 - bkp dia

--- commit_git.sh
+++ commit_git.sh
@@ -4,5 +4,5 @@
 #linha abaixo -all varre todos arquivos modificados
 git add --all
 #colocar abaixo a msg do q foi alterado antes de comitar...
-git commit -m "25nov21 - bkp dia"
+git commit -m "02dez21 - bkp dia"
 git push -u origin main
